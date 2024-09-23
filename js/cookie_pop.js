81b762c749a7de6957dfb76e7ca653c15d1fa543
khi8194 <khi8194@gmail.com>
2024-09-23 16:11:24 +0900

    쿠키의 만료기한을 second, date, minutes, hour등으로 설정하면 안되는 이유

--- js/cookie_pop.js
+++ js/cookie_pop.js
@@ -13,20 +13,44 @@
   쿠키확인 : document.cookie
 */
 
+// const [btnView, btnSet] = document.querySelectorAll("button");
+// btnView.addEventListener("click", () => {
+// 	console.log(document.cookie);
+// });
+// btnSet.addEventListener("click", () => {
+// 	setCookie("today", "done", 5);
+// });
+// //쿠키 생성 함수
+// function setCookie(name, value, sec) {
+// 	let now = new Date();
+// 	//현재 분값을 가져와서 인수로 전달된 분 시간정보를 더함
+// 	let duedate = now.getSeconds() + sec;
+// 	//바뀐 시간 정보값으로 시간객체정보를 변경
+// 	now.setSeconds(duedate);
+// 	//변경된 시간 정보값을 표준시로 변경해서 쿠키만료시간으로 설정
+// 	document.cookie = `${name}=${value}; path=/; expires=${now.toUTCString()}`;
+// }
+
 const [btnView, btnSet] = document.querySelectorAll("button");
+
 btnView.addEventListener("click", () => {
 	console.log(document.cookie);
 });
+
 btnSet.addEventListener("click", () => {
-	setCookie("today", "done", 5);
+	//today=done이라는 이름으로 쿠키 생성함과 동시에 만료시간을 1분을 지정하여
+	//쿠키가 생성된 시점부터 1분까지만 유지되고 1분뒤에는 자동으로 쿠키 제거됨
+	setCookie("today", "done", 1);
 });
+
 //쿠키 생성 함수
-function setCookie(name, value, sec) {
+function setCookie(name, value, min) {
 	let now = new Date();
 	//현재 분값을 가져와서 인수로 전달된 분 시간정보를 더함
-	let duedate = now.getSeconds() + sec;
+	let duedate = now.getMinutes() + min;
 	//바뀐 시간 정보값으로 시간객체정보를 변경
-	now.setSeconds(duedate);
+	now.setMinutes(duedate);
 	//변경된 시간 정보값을 표준시로 변경해서 쿠키만료시간으로 설정
 	document.cookie = `${name}=${value}; path=/; expires=${now.toUTCString()}`;
+	alert("쿠키 생성");
 }
