f1c8fcade79a6786525e1c5db3ff38cc537b3d64
khi8194 <khi8194@gmail.com>
2024-09-23 16:54:50 +0900

    쿠키값 갱신은 화면을 새로고침 시 반영

--- js/cookie_pop.js
+++ js/cookie_pop.js
@@ -40,9 +40,10 @@ btnView.addEventListener("click", () => {
 btnSet.addEventListener("click", () => {
 	//today=done이라는 이름으로 쿠키 생성함과 동시에 만료시간을 1분을 지정하여
 	//쿠키가 생성된 시점부터 1분까지만 유지되고 1분뒤에는 자동으로 쿠키 제거됨
-	setCookie("today", "done", 1);
+	setCookie("today", "done", 10);
 });
 
+/*
 //쿠키 생성 함수
 function setCookie(name, value, min) {
 	let now = new Date();
@@ -54,7 +55,14 @@ function setCookie(name, value, min) {
 	document.cookie = `${name}=${value}; path=/; expires=${now.toUTCString()}`;
 	alert("쿠키 생성");
 }
+*/
 
-//위와 같이 쿠키의 만료시간을 second, minutes, date로 지정하면
-//쿠키가 생성도는 시점에 만료없이 설정되다보니 64초, 65분, 35일 같은 존재하지 않는 값으로 만료일이 잘못 세팅되는 이슈발생
-//해결방법은 getTime()을 이용해서: 표준시가 설정된 이후부터를 초단위로 연산해서 반환된 값으로 설정
+function setCookie(name, value, sec) {
+	let now = new Date();
+	//아래와 같이 getTime으로 현재 시간값을 가져와서 초단위로 값을 변경하면, 잘못된 날짜 정보로 만료일 설정을 예방
+	let duedate = now.getTime() + sec * 1000;
+	//쿠키값은 화면 새로고침되어야지만 갱신된값이 반영
+	now.setTime(duedate);
+	document.cookie = `${name}=${value}; path=/; expires=${now.toUTCString()}`;
+	alert("쿠키 생성");
+}
